ae28c699c74495e815aa525685e57c90fe344e11
Glitch (hello-express) <none>
2019-06-29 12:06:54 +0000

    👝💾 Checkpoint ./watch.json:5053261/260 ./app/template.html:5053261/128 ./app/app.js:5053261/414

--- app/app.js
+++ app/app.js
@@ -2,6 +2,15 @@ $(document).ready( function() {
     app.initialized()
         .then(function(_client) {
           var client = _client;
+      client.interface.trigger("showNotify", {
+        type: "success",
+        message: "sample notification"
+      /* The "message" should be plain text */
+      }).then(function(data) {
+      // data - success message
+      }).catch(function(error) {
+      // error - error object
+      });
           client.events.on('app.activated',
             function() {
                 client.data.get('loggedInAgent')
