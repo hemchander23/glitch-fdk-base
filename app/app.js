2106f0a9ba5c17e831e33481bfdb95c9e4009995
Glitch (freshchat-hello-world) <none>
2019-07-24 09:16:49 +0000

    🎋👾 Checkpoint ./app/template.html:5053261/1290 ./app/app.js:5053261/4754

--- app/app.js
+++ app/app.js
@@ -2,8 +2,7 @@ $(document).ready( function() {
   app.initialized()
     .then(function(_client) {
     var client = _client;
-    client.events.on('app.activated',
-                     function() {
+    client.events.on('app.activated',function() {
       client.data.get('loggedInAgent')
         .then(function(data) {
         $('#apptext').text("We have an agent logged in and it is: " + data.loggedInAgent.email);
