10815fa9a6cac3b52e7e5658f1d6740fbf473172
Glitch (fdk6) <none>
2020-04-14 16:05:44 +0000

    🐘🏅 Checkpoint ./workspace/server/server.js:5053261/3207

--- workspace/server/server.js
+++ workspace/server/server.js
@@ -1,6 +1,6 @@
-const helper = require("./helper");
+const helper = require('./helper');
 
-const PREFIX = "convo-";
+const PREFIX = 'convo-';
 
 /**
  * To retrieve the note from the data storage.
@@ -8,36 +8,29 @@ const PREFIX = "convo-";
  * @param {String} conversationId - The conversation_id of the Agent conversation.
  **/
 function retrieveNote(conversationId) {
-  $db.get(`${PREFIX}${conversationId}`).then(
-    function(data) {
-      console.log(data);
-    },
-    function(err) {
-      console.log(err);
-    }
-  );
+  return $db.get(`${PREFIX}${conversationId}`);
 }
 
 exports = {
+
   /**
-   * To get the stored note.
-   *
-   * @param {String} conversationId - The conversation_id of the Agent conversation.
-   **/
-  getNote: function(payload) {
+  * To get the stored note.
+  *
+  * @param {String} conversationId - The conversation_id of the Agent conversation.
+  **/
+  getNote: function (payload) {
     const encodedConversationId = helper.encode(payload.conversation_id);
-    console.log(encodedConversationId);
     retrieveNote(encodedConversationId).then(
-      function(data) {
-        console.info("Seccessfully fetched the note data from db");
+      function (data) {
+        console.info('Seccessfully fetched the note data from db');
         console.info(data);
         renderData(null, data);
       },
-      function(error) {
-        console.error("failed to store the key-value pair successfully");
+      function (error) {
+        console.error('failed to store the key-value pair successfully');
         console.error(error);
         renderData(error);
-      }
-    );
+      });
   }
-};
+}
+
